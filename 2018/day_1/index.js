8108d6c7f7588fef682fc9d4bf3fd404e3193019
John Munn <jmunn@getchalk.com>
2018-12-01 23:22:40 -0400

    Day 1 part 2 solved

--- 2018/day_1/index.js
+++ 2018/day_1/index.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
-const numbers = fs.readFileSync('input.txt').toString().split("\n");
-const total = numbers.reduce((a, b) => Number(a) + Number(b), 0);
+const numbers = fs.readFileSync('input.txt').toString().split("\n").map(Number);
 
-console.log(`The total is: ${total}`)
+/* Part one */
+const total = numbers.reduce((a, b) => Number(a) + Number(b), 0);
+console.log(`Part One: The total is ${total}`)
+
+/* Part Two */
+const frequencies = new Set();
+let frequency = 0;
+
+while (true) {
+    for (const num of numbers) {
+        frequencies.add(frequency);
+        frequency += num;
+        if (frequencies.has(frequency)) {
+            console.log(`Part Two: The frequency is ${frequency}`)
+            return;
+        }
+    }
+}
