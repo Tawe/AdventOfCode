ce7cfe4ac41d10a30b9344036ae8d7f7e0a5859a
John Munn <jmunn@getchalk.com>
2018-12-03 22:45:18 -0400

    Removing unneed split

--- 2018/day_2/index.js
+++ 2018/day_2/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const arrs = fs.readFileSync('input.txt').toString().split("\n");
+
+/* Part One */
 let hasThree = 0;
 let hasTwo = 0;
-const letterArrs  = arrs.map(arr=>arr.split(''))
-const countArray = letterArrs.map(letters=>letters.map(letter=>letters.filter(x=>x===letter).length))
-countArray.map(arr=>{
+const letterArrs  = arrs.map(arr=>[...arr])
+const countArrs = letterArrs.map(letters=>letters.map(letter=>letters.filter(x=>x===letter).length))
+countArrs.map(arr=>{
     if(arr.includes(2)) {
       hasTwo++
     }
@@ -15,3 +17,4 @@ countArray.map(arr=>{
 
 const checksum = (hasTwo*hasThree);
 console.log(`Part one: the checksum is ${checksum}`)
+
