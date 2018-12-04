a43669724fb25a58703af68c0d1b5a3f87b39491
John Munn <jmunn@getchalk.com>
2018-12-04 03:07:01 -0400

    Day 2 part 2 solved

--- 2018/day_2/index.js
+++ 2018/day_2/index.js
@@ -18,3 +18,24 @@ countArrs.map(arr=>{
 const checksum = (hasTwo*hasThree);
 console.log(`Part one: the checksum is ${checksum}`)
 
+/* Part Two */
+let letterDiff = null;
+let compareStringOne = ''
+let compareStringTwo = ''
+
+
+arrs.map(firstArray=> {
+  arrs.map(secondArray=>{
+    if(firstArray !== secondArray){
+      const diff = [...firstArray].filter((letter,index)=>[...secondArray][index] !== letter).join('').length;
+      if(!letterDiff || diff < letterDiff){
+        letterDiff = diff;
+        compareStringOne = firstArray;
+        compareStringTwo = secondArray;
+      }
+    }
+  })
+})
+
+  const boxId = [...compareStringOne].filter((letter,index)=>letter===compareStringTwo[index]).join('')
+  console.log(`Part two: the box id is ${boxId}`)
