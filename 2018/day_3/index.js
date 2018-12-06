2dcfa491014f927e30669023dc1fc0e0be4cd57b
John Munn <jmunn@getchalk.com>
2018-12-06 00:14:16 -0400

    Day 3 Part 2 solved

--- 2018/day_3/index.js
+++ 2018/day_3/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString().split("\n");
 
 let cells = {};
+let fabric = {};
 let claims = {};
 
 
@@ -19,15 +20,19 @@ claimArray.map(claim=>{
    claims[claim.id] = true;
    for (let x = claim.left; x < claim.left+claim.width; x++) {
       for (let y = claim.top; y < claim.top+claim.height; y++) {
-          const dim = [`${x},${y},${claim.id}`];
-         if (cells[dim]) {
-            claims[cells[dim]] = false;
+        const dim = [`${x},${y}`];
+         if (fabric[dim]) {
+            claims[fabric[dim]] = false;
             claims[claim.id] = false;
          }
+
+         fabric[dim] = claim.id;
          cells[dim] = (cells[dim] || 0) + 1;
       }
    }
 })
-console.log(Object.entries(claims).filter(v => v[1]));
+
+const freeClaim = Object.entries(claims).filter(v => v[1]);
 const count = Object.values(cells).filter(v => v > 1).length
 console.log(`Part One: ${count} inches of fabric are claimed`);
+console.log(`Part Two: #${freeClaim[0][0]} is the only claim with no overlap`);
