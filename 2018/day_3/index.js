f6eec5d288eadd2334fad42868a5933b20e55b17
John Munn <jmunn@getchalk.com>
2018-12-06 00:13:51 -0400

    Day 3 Part 2 solved

--- 2018/day_3/index.js
+++ 2018/day_3/index.js
@@ -1,26 +1,33 @@
 const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString().split("\n");
 
-/* Part One */
-const cells = {};
+let cells = {};
+let claims = {};
+
+
 const claimArray = [...input].map(item=>item.match(/\d{0,}/g).filter(x=>x)).map(claim=> 
-({
-    id:Number(claim[0]),
-    left: Number(claim[1]), 
-    top: Number(claim[2]), 
-    width: Number(claim[3]), 
-    height: Number(claim[4])
-}))
+    ({
+        id:Number(claim[0]),
+        left: Number(claim[1]), 
+        top: Number(claim[2]), 
+        width: Number(claim[3]), 
+        height: Number(claim[4])
+    }))
+    
 
 claimArray.map(claim=>{
-   for (let left = claim.left; left < claim.left + claim.width; left++) {
-      for (let top = claim.top; top < claim.top + claim.height; top++) {
-         cells[`${left},${top}`] = (cells[`${left},${top}`] || 0) + 1;
+   claims[claim.id] = true;
+   for (let x = claim.left; x < claim.left+claim.width; x++) {
+      for (let y = claim.top; y < claim.top+claim.height; y++) {
+          const dim = [`${x},${y},${claim.id}`];
+         if (cells[dim]) {
+            claims[cells[dim]] = false;
+            claims[claim.id] = false;
+         }
+         cells[dim] = (cells[dim] || 0) + 1;
       }
    }
 })
-
+console.log(Object.entries(claims).filter(v => v[1]));
 const count = Object.values(cells).filter(v => v > 1).length
 console.log(`Part One: ${count} inches of fabric are claimed`);
-
-
