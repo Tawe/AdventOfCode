74629085208037cb25aa1badddf706759a61d88d
John Munn <jmunn@getchalk.com>
2019-12-01 20:06:36 -0400

    Day 1 2019

---
+++ 2019/day_1/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const numbers = fs.readFileSync('input.txt').toString().split("\n").map(Number);
+
+
+const value = (val) => Math.floor(val/3)-2
+
+const fuelArr = arr => arr.map(num => value(num));
+const sumVals = (arr) => arr.reduce((acc, cur)=>acc+cur)
+
+//Part One
+console.log(sumVals(fuelArr(numbers))) //3282935
+
+const totalFuel = fuelArr(numbers).map(fuel =>{
+    let total = fuel;
+     while(value(fuel)>=0){
+        fuel = value(fuel)
+        total = fuel+total;
+     };
+     return total;
+})
+
+
+//Part Two
+console.log(sumVals(totalFuel));
