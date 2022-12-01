1848b0e4971f72082839faeedcf8964299352f0e
John Munn <jmunn@getchalk.com>
2022-12-01 16:59:05 -0400

    fixing questions

--- 2022/day_1/index.js
+++ 2022/day_1/index.js
@@ -3,5 +3,5 @@ const caloriesArr = fs.readFileSync('input.txt').toString().split(/\n\s*\n/).map
 
 const sumedCalArr = caloriesArr.map(cal=>cal.reduce((a, b) => a + b, 0)).sort((a,b)=>a-b).reverse();
 
-console.log(`How many total Calories is that Elf carrying? ${sumedCalArr[0]}`)
-console.log(`How many Calories are the top three elves carrying in total? ${sumedCalArr[0]+sumedCalArr[1]+sumedCalArr[2]}`);
+console.log(`How many total calories is the elf with the must calories carrying? ${sumedCalArr[0]}`)
+console.log(`How many calories are the top three elves carrying in total? ${sumedCalArr[0]+sumedCalArr[1]+sumedCalArr[2]}`);
