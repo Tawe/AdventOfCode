1fdbb428fca55e1c6e57ff8ab4449265461cf806
John Munn <jmunn@getchalk.com>
2022-12-01 10:31:08 -0400

    Fixing spacing

--- 2022/day_1/index.js
+++ 2022/day_1/index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const caloriesArr = fs.readFileSync('input.txt').toString().split(/\n\s*\n/).map(cal => cal.replace(/\n/g,',').split(',').map(Number));
 
+const sumedCalArr = caloriesArr.map(cal=>cal.reduce((a, b) => a + b, 0)).sort((a,b)=>a-b).reverse();
 
-const sumCalArr = caloriesArr.map(cal=>cal.reduce((a, b) => a + b, 0)).sort((a,b)=>a-b).reverse();
-
-
-console.log(`How many total Calories is that Elf carrying? ${sumCalArr[0]}`)
-console.log(`How many Calories are the top three elves carrying in total? ${sumCalArr[0]+sumCalArr[1]+sumCalArr[2]}`);
+console.log(`How many total Calories is that Elf carrying? ${sumedCalArr[0]}`)
+console.log(`How many Calories are the top three elves carrying in total? ${sumedCalArr[0]+sumedCalArr[1]+sumedCalArr[2]}`);
