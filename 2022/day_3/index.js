36d0453f18e170ba8e7d2698f997dca1ef2a15eb
John Munn <jmunn@getchalk.com>
2022-12-03 12:53:33 -0400

    day_3 part 2

--- 2022/day_3/index.js
+++ 2022/day_3/index.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
 const rucksackArr = fs.readFileSync('input.txt').toString().split("\n");
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const firdMatching = () => rucksackArr.map(rucksack => {
-    const [...firstPocket] = rucksack.slice(0, rucksack.length/2)
-    const [...secondPocket] = rucksack.slice(rucksack.length / 2, rucksack.length)
-    const matching = firstPocket.filter(letter => secondPocket.includes(letter));
-    return matching   
+const findMatching = (arr) => arr.map(rucksack => {
+    const [...firstCompartment] = rucksack.slice(0, rucksack.length/2)
+    const [...secondCompartment] = rucksack.slice(rucksack.length / 2, rucksack.length)
+    return firstCompartment.filter(letter => secondCompartment.includes(letter));  
 })
 
-const findSum = (arr) => arr.map(letterArr=>{
-    const letter = letterArr[0];
-    const isUppercase = /^[A-Z]*$/.test(letter)
-    if(isUppercase){
-        const lowerCase = letter.toLowerCase();
-        return alphabet.indexOf(lowerCase) + 27;
-    }
-    console.log(letter, alphabet.indexOf(letter))
-    return alphabet.indexOf(letter)+1;
-}).reduce((a, b) => a + b, 0);
+const findSum = (arr) => arr.map(letterArr=> alphabet.indexOf(letterArr[0])+1).reduce((a, b) => a + b, 0);
 
-console.log(`Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types? ${findSum(firdMatching())}`); // 7997
+const findBadges = () => {
+    let badgeArr = [];
+    for (let i = 0; i < rucksackArr.length; i += 3) {
+        badgeArr.push(compareRuckSacks([...rucksackArr[i]], [...rucksackArr[i+1]], [...rucksackArr[i+2]]));
+    }
+    return badgeArr;
+}
+
+const compareRuckSacks = (rucksackOne, rucksackTwo, rucksackThree) => {
+    const firstCompare = rucksackOne.filter(letter => rucksackTwo.includes(letter));
+    return rucksackThree.filter(letter => firstCompare.includes(letter))[0];
+}
+
+console.log(`Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types? ${findSum(findMatching(rucksackArr))}`); // 7997
+console.log(`Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types? ${findSum(findBadges())}`)  ; //2545
